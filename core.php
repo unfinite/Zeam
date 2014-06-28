ae866f00b607dfff4baab0c24201ba1f6b14a3b8
Nathan Johnson <nathancj224@gmail.com>
2014-06-28 00:21:54 -0400

    cleaned up core.php a bit; added a log() method

--- core.php
+++ core.php
@@ -1,25 +1,25 @@
-<?php 
+<?php
 
 /*
 
 
 
- ________  _______   ________  _____ ______      
-|\_____  \|\  ___ \ |\   __  \|\   _ \  _   \    
- \|___/  /\ \   __/|\ \  \|\  \ \  \\\__\ \  \   
-     /  / /\ \  \_|/_\ \   __  \ \  \\|__| \  \  
-    /  /_/__\ \  \_|\ \ \  \ \  \ \  \    \ \  \ 
+ ________  _______   ________  _____ ______
+|\_____  \|\  ___ \ |\   __  \|\   _ \  _   \
+ \|___/  /\ \   __/|\ \  \|\  \ \  \\\__\ \  \
+     /  / /\ \  \_|/_\ \   __  \ \  \\|__| \  \
+    /  /_/__\ \  \_|\ \ \  \ \  \ \  \    \ \  \
    |\________\ \_______\ \__\ \__\ \__\    \ \__\
     \|_______|\|_______|\|__|\|__|\|__|     \|__|
-                                                 
-                                                 
+
+
 
 Copyright 2014 Alphasquare.us
 Licensed with the MIT license
 http://github.com/Alphasquare/Zeam
 http://alphasquare.us/code
 
-You can remove this, but we'd love you to death if you kept this here. 
+You can remove this, but we'd love you to death if you kept this here.
 Also, we'd marry you if you put a link back to Alphasquare.us.
 
 */
@@ -33,7 +33,7 @@ class Zeam {
 	private $password;
 	protected $logging;
 
-	function __construct($db, $host, $user, $password, $logging = false) {
+	public function __construct($db, $host, $user, $password, $logging = false) {
 
 
 		$this->db = $db;
@@ -45,43 +45,52 @@ class Zeam {
 
 		if ($this->logging) {
 			error_reporting(E_ALL);
-			echo ''.$this->prefix.'<i>Object created. Everything /seems/ fine. Logging is on. Setting Zeam logging on will also set PHP\'s native error reporting into E_ALL mode. Make sure the variables are correct! Please run the start() method of the class now. </i><br>';
-		} else {
+			$this->log('Object created. Everything /seems/ fine. Logging is on. Setting Zeam logging on will also set PHP\'s native error reporting into E_ALL mode. Make sure the variables are correct! Please run the start() method of the class now.');
+		}
+		else {
 			error_reporting(0);
 		}
 
 	}
 
-	function start() {
-		
+	public function start() {
+
 		global $mysqli;
 		$mysqli = new mysqli($this->host, $this->user, $this->password, $this->db);
 
-		if (mysqli_connect_errno()) {
-    		echo ''.$this->prefix.' <i>Could not establish a connection with the database. Please check your variables. For detailed information, refer to PHP error reporting.</i><br>';
-    		} else {
-    		if ($this->logging == "yes") {
-			echo ''.$this->prefix.'<i>Connection established.</i><br>';
-		}
-    	}
+		if ($mysqli->connect_errno) {
+    	$this->log('Could not establish a connection with the database. Please check your variables. For detailed information, refer to PHP error reporting.');
+    }
+    else {
+    	$this->log('Connection established.');
+    }
 
 	}
-	
-	function hash($type, $content) {
+
+	public function hash($type, $content) {
 		switch ($type) {
 			case hash_types::MD5:
 				return md5($content);
-				break;
+			break;
 			case hash_types::SHA1:
 				return sha1($content);
-				break;
+			break;
 			case hash_types::CRYPT:
 				return crypt($content);
-				break;
+			break;
 			default:
-				throw new Exception("Unknown hash type."); // We should probably use a custom Exception class in the future but this'll work for now.
+				// We should probably use a custom Exception class in the future but this'll work for now.
+				throw new Exception("Unknown hash type.");
 		}
 	}
+
+	private function log($message) {
+		if(!$this->logging) return false;
+		echo $this->prefix;
+		echo '<em>' . $message . '</em>';
+		echo '<br />';
+	}
+
 }
 
 class hash_types {
@@ -89,3 +98,5 @@ class hash_types {
 	const SHA1 = 1;
 	const CRYPT = 2;
 }
+
+/* End of file: core.php */
