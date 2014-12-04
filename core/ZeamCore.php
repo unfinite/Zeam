3a0c7cd09729e5d6f41ca061bc495ed732a7493c
Sergio Diaz <Alphapixels@users.noreply.github.com>
2014-12-03 22:20:59 -0400

    Added configs array support

--- core/ZeamCore.php
+++ core/ZeamCore.php
@@ -4,9 +4,36 @@
  * Starting point for everything Zeam.
  */
 class ZeamCore {
-    
-    // WIP
-    
+
+	public $version;
+	public $configs;
+	public $app;
+
+	function init() {
+
+		// framework includes and essentials
+
+	}
+
+	public function setSettings($array) {
+
+		if (isset($array['ZEAM_BASEPATH']) && isset($array['ZEAM_COREPATH']) && isset($array['ZEAM_MODULEPATH'])) {		
+
+			$this->configs = $array;  // Store configs array in ZeamCore
+
+			foreach ($array as $k => $v) {
+
+				define($k, $v);
+
+			}
+
+		} else {
+
+			throw new Exception("One essential value is not defined. Please check the settings array.");
+		}
+
+	}
+	
 }
 
 ?>
